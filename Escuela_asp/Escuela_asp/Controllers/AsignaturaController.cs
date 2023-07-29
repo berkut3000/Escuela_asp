e9ba02c53550d9bb42b7082dac6c725a1ff49ac4
Antonio Aguilar Mota <berkut3000@hotmail.com>
2023-07-29 00:21:04 -0600

    24 Ajuste de Enrutamiento

--- Escuela_asp/Escuela_asp/Controllers/AsignaturaController.cs
+++ Escuela_asp/Escuela_asp/Controllers/AsignaturaController.cs
@@ -5,10 +5,24 @@ namespace Escuela_asp.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index()
+        [Route("Asignatura/Index")]
+        [Route("Asignatura/Index/{asignaturaId}")]
+
+        public IActionResult Index(string asignaturaId)
         {
-            return View(_context.Asignaturas.FirstOrDefault());
+            if(!string.IsNullOrEmpty(asignaturaId))
+            {
+                var asignatura = from asig in _context.Asignaturas
+                                 where asig.Id == asignaturaId
+                                 select asig;
+                return View(asignatura.SingleOrDefault());
+            }
+            else
+            {
+                return View("Multiasignatura", _context.Asignaturas);
+            }
         }
+
         public IActionResult Multiasignatura()
         {
             ViewBag.CosaDinamica = "La Monja";
