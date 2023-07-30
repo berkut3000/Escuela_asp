1049112f94c71475949b3445a555ffc5bf42844c
Antonio Aguilar Mota <berkut3000@hotmail.com>
2023-07-30 13:46:36 -0600

    25 Ajuste de Vistas y Controladores

--- Escuela_asp/Escuela_asp/Controllers/AlumnoController.cs
+++ Escuela_asp/Escuela_asp/Controllers/AlumnoController.cs
@@ -6,9 +6,23 @@ namespace Escuela_asp.Controllers
 {
     public class AlumnoController : Controller
     {
-        public IActionResult Index()
+        [Route("[controller]")]
+        [Route("Alumno/Index")]
+        [Route("Alumno/Index/{id}")]
+        [Route("Alumno/{id}")]
+        public IActionResult Index(string? id)
         {
-            return View(_context.Alumnos.FirstOrDefault());
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                var alumno = from alumn in _context.Alumnos
+                             where alumn.Id == id
+                             select alumn;
+                return View(alumno.SingleOrDefault());
+            }
+            else
+            {
+                return View("Multialumno", _context.Alumnos);
+            }
         }
         public IActionResult Multialumno()
         {
---
+++ Escuela_asp/Escuela_asp/Controllers/CursoController.cs
@@ -0,0 +1,39 @@
+﻿using Escuela_asp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Escuela_asp.Controllers
+{
+    public class CursoController : Controller
+    {
+        [Route("[controller]")]
+        [Route("Curso/Index")]
+        [Route("Curso/Index/{id}")]
+        [Route("Curso/{id}")]
+        public IActionResult Index(string? id)
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                var Curso = from curs in _context.Cursos
+                             where curs.Id == id
+                             select curs;
+                return View(Curso.SingleOrDefault());
+            }
+            else
+            {
+                return View("MultiCurso", _context.Cursos);
+            }
+        }
+        public IActionResult MultiCurso()
+        {
+            return View("MultiCurso", _context.Cursos);
+        }
+
+        private EscuelaContext _context;
+        public CursoController(EscuelaContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
